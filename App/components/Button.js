37d97e47107e776772dee391a5ce4578eedb85a7
edhernandez04 <mchh.004@gmail.com>
2020-07-28 19:29:45 -0400

    initial complete

---
+++ App/components/Button.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { TouchableOpacity, Text, StyleSheet, Dimensions } from 'react-native'
+
+export default ({ onPress, text, size, theme }) => {
+    const buttonStyles = [styles.button]
+    const textStyles = [styles.text]
+
+    if (size === 'double') {
+        buttonStyles.push(styles.buttonDouble)
+    }
+
+    if (theme === 'secondary'){
+        buttonStyles.push(styles.buttonSecondary)
+        textStyles.push(styles.textSecondary)
+    } else if (theme === 'accent'){
+        buttonStyles.push(styles.buttonAccent)
+    }
+
+    return (
+        <TouchableOpacity onPress={onPress} style={buttonStyles}>
+            <Text style={textStyles}>{text}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const screen =  Dimensions.get('window')
+const buttonWidth = screen.width / 4
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'white',
+        fontSize: 25
+    },
+    textSecondary: {
+        color: '#060606'
+    },
+    button: {
+        backgroundColor: '#333333',
+        flex: 1,
+        height: Math.floor(buttonWidth - 10),
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: Math.floor(buttonWidth),
+        margin: 5
+    },
+    buttonDouble: {
+        width: screen.width / 2 - 10,
+        flex: 0,
+        alignItems: "flex-start",
+        paddingLeft: 40
+    },
+    buttonSecondary: {
+        backgroundColor: '#a6a6a6'
+    },
+    buttonAccent: {
+        backgroundColor: '#f09a36'
+    }
+})
